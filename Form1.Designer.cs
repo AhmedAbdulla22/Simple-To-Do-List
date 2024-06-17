2e3ac15d89da1bdfd0758d2820107d8967e3b3c2
Ahmed Abdullah <ahmeddev452@gmail.com>
2024-06-17 22:36:15 +0300

    Modifing the Text box to Add Items

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,23 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.clbToDoList = new System.Windows.Forms.CheckedListBox();
+            this.tbAddItems = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // clbToDoList
+            // 
+            this.clbToDoList.Font = new System.Drawing.Font("Cambria", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clbToDoList.FormattingEnabled = true;
+            this.clbToDoList.Items.AddRange(new object[] {
+            "Add",
+            "DELETE"});
+            this.clbToDoList.Location = new System.Drawing.Point(12, 84);
+            this.clbToDoList.Name = "clbToDoList";
+            this.clbToDoList.Size = new System.Drawing.Size(349, 277);
+            this.clbToDoList.TabIndex = 0;
+            // 
+            // tbAddItems
+            // 
+            this.tbAddItems.Location = new System.Drawing.Point(12, 58);
+            this.tbAddItems.Name = "tbAddItems";
+            this.tbAddItems.Size = new System.Drawing.Size(349, 20);
+            this.tbAddItems.TabIndex = 1;
+            this.tbAddItems.Text = "Type & Enter to Add Items";
+            this.tbAddItems.Enter += new System.EventHandler(this.tbAddItems_Enter);
+            this.tbAddItems.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbAddItems_KeyPress);
+            this.tbAddItems.Leave += new System.EventHandler(this.tbAddItems_Leave);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(373, 524);
+            this.Controls.Add(this.tbAddItems);
+            this.Controls.Add(this.clbToDoList);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckedListBox clbToDoList;
+        private System.Windows.Forms.TextBox tbAddItems;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,31 @@ namespace Simple_To_Do_List
         {
             InitializeComponent();
         }
+
+        private void tbAddItems_Enter(object sender, EventArgs e)
+        {
+            if (tbAddItems.Text == "Type & Enter to Add Items")
+            {
+            tbAddItems.Text = "";
+            }
+
+        }
+
+        private void tbAddItems_Leave(object sender, EventArgs e)
+        {
+            if (tbAddItems.Text == "")
+            {
+            tbAddItems.Text = "Type & Enter to Add Items";
+            }
+        }
+
+        private void tbAddItems_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                clbToDoList.Items.Add(tbAddItems.Text);
+                tbAddItems.Text = string.Empty;
+            }
+        }
     }
 }
